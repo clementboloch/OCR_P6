da694d6ddbb2844fe5eb9582df6bc666dd05433e
clementboloch <clement.boloch@gmail.com>
2023-01-15 15:13:31 +0100

    update script

--- draft/script.js
+++ draft/script.js
@@ -34,11 +34,12 @@ class Movie {
   }
 
   class Category {
-    constructor(dic, limit = 20) {
+    constructor(dic, limit = 5) {
         this.id = dic.id;
-        console.log("this.id", this.id);
         this.name = dic.name;
         this.limit = limit;
+        this.nbMovies = 0;
+        // this.firstMovie = 1;
 
         this.generateElement();
     }
@@ -50,9 +51,9 @@ class Movie {
         const name = document.createTextNode(this.name);
         nameContainer.appendChild(name);
         
-        const movieContainer = document.createElement("div");
-        movieContainer.classList.add('movie-container');
-        movieContainer.setAttribute("id", `movies-cat${this.id}`);
+        this.movieContainer = document.createElement("div");
+        this.movieContainer.classList.add('movie-container');
+        this.movieContainer.setAttribute("id", `movies-cat${this.id}`);
         
         const nextContainer = document.createElement("div");
         nextContainer.classList.add('next');
@@ -60,20 +61,55 @@ class Movie {
         nextImage.classList.add('next-icon');
         nextImage.setAttribute("src", "img/icon/arrow_right.svg");
         nextImage.setAttribute("alt", "next");
-        nextContainer.addEventListener("click", this.rightSlide);
+        nextContainer.addEventListener("click", () => this.slide(this, "rigth"));
         
         categoryContainer.appendChild(nameContainer);
-        categoryContainer.appendChild(movieContainer);
-        movieContainer.appendChild(nextContainer);
+        categoryContainer.appendChild(this.movieContainer);
+        this.movieContainer.appendChild(nextContainer);
         nextContainer.appendChild(nextImage);
 
         document.body.appendChild(categoryContainer);
     }
 
-    rightSlide() {
-        const movieContainer = document.getElementById(`movies-cat${this.id}`);
-        console.log("this.id", this.id);
-        movieContainer.style.marginLeft = "0";
+    addMovie(film) {
+        new Movie(film, this.id);
+        this.nbMovies += 1;
+    }
+
+    async addMovies() {
+        for (let page = 1; page <= this.limit; page++) {
+            const response = await fetch(`http://localhost:8000/api/v1/titles/?genre=${this.name}&page=${page}`);
+            const myJson = await response.json(); 
+            
+            const films = myJson.results
+            for (const film of films) {
+                this.addMovie(film);
+            }
+    
+            if (myJson.next == null) break;
+        }
+        this.lastMovie = this.nbMovies >= 6 ? 6 : this.nbMovies;
+        console.log("this.lastMovie", this.lastMovie)
+    }
+
+    slide(category, side) {
+        const movie = document.getElementsByClassName("movie")[0];
+        const movieStyle = window.getComputedStyle(movie);
+        const movieWidth = parseFloat(movieStyle.width);
+        const movieMarginRight = parseFloat(movieStyle.marginRight);
+        
+        const oldPosition = parseFloat(window.getComputedStyle(category.movieContainer).marginLeft);
+        let newPosition;
+        if (side == "rigth") {
+            const slideSize = this.lastMovie + 6 <= this.nbMovies ? 6 : this.nbMovies - this.lastMovie;
+            this.lastMovie += slideSize;
+            newPosition = oldPosition - slideSize * (movieWidth + movieMarginRight);
+        } else {
+            const slideSize = this.firstMovie - 6 <= 0 ? 6 : this.firstMovie;
+            this.firstMovie -= slideSize;
+            newPosition = oldPosition + slideSize * (movieWidth + movieMarginRight);
+        }
+        category.movieContainer.style.marginLeft = `${newPosition}px`;
     }
   }
 
@@ -83,23 +119,9 @@ class Movie {
     const myJson = await response.json(); 
     const categories = myJson.results;
     
-    
-    const limit = 20;
-    
     for (const category of categories) {
-        new Category(category);
-        
-        for (let page = 1; page <= limit; page++) {
-            const response = await fetch(`http://localhost:8000/api/v1/titles/?genre=${category.name}&page=${page}`);
-            const myJson = await response.json(); 
-            
-            const films = myJson.results
-            for (const film of films) {
-                new Movie(film, category.id);
-            }
-    
-            if (myJson.next == null) break;
-        }
+        const newCategory = new Category(category);
+        newCategory.addMovies();
     }
 }
 
